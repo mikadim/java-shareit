dadde23f3b6a182470e83fa8eb38662321e19262
DM <mikadim@yandex.ru>
2023-03-10 00:05:39 +0300

    fix: new class BookingItemDto for ItemDto

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -2,26 +2,25 @@ package ru.practicum.shareit.booking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import ru.practicum.shareit.booking.dto.BookingForItemDto;
 import ru.practicum.shareit.booking.model.Booking;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    public Optional<Booking> findByIdAndItemOwner(Long bookingId, Long ownerId);
+    Optional<Booking> findByIdAndItemOwner(Long bookingId, Long ownerId);
 
     @Query(" select b from Booking b " +
             "inner join b.item i " +
             "inner join b.booker u " +
             "where b.id = ?1 and (i.owner = ?2 or u.id = ?2)")
-    public Optional<Booking> findByIdAndBookerIdAndItemOwner(Long bookingId, Long userId);
+    Optional<Booking> findByIdAndBookerIdAndItemOwner(Long bookingId, Long userId);
 
-    public List<Booking> findByBookerId(Long bookerId);
+    List<Booking> findByBookerId(Long bookerId);
 
-    public List<Booking> findByItemOwner(Long ownerId);
+    List<Booking> findByItemOwner(Long ownerId);
 
-    public List<Booking> findByItemIdAndItemOwner(Long itemId, Long userId);
+    List<Booking> findByItemIdAndItemOwner(Long itemId, Long userId);
 
-    public List<Booking> findByItemIdAndBookerId(Long itemId, Long bookerId);
+    List<Booking> findByItemIdAndBookerId(Long itemId, Long bookerId);
 }
