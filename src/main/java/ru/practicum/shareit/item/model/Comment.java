522063511fbdebc372edcaecd62d55cd6c4bdf79
DM <mikadim@yandex.ru>
2023-03-17 03:11:23 +0300

    test7

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -27,6 +27,6 @@ public class Comment {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "author_id", referencedColumnName = "id")
     private User author;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SS")
     private LocalDateTime created;
 }
