da22a9b21a7c6a37df6a5eac6b839c40c4b41739
DM <mikadim@yandex.ru>
2023-03-28 23:01:31 +0300

    test2

--- server/src/test/java/ru/practicum/shareit/user/UserControllerIT.java
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerIT.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
